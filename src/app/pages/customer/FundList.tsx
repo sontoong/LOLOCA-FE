f54e6a4d0007eb227aa07a51abbcebaf5d0100fc
EShroomThe1st <xxxthedestroyer058xxx@gmail.com>
2024-07-14 03:03:55 +0700

    Payment Table

--- src/app/pages/customer/FundList.tsx
+++ src/app/pages/customer/FundList.tsx
@@ -2,54 +2,54 @@ import { PlusOutlined } from "@ant-design/icons";
 import { PrimaryButton } from "../../components/buttons";
 import OutlineButton from "../../components/buttons/outline-button";
 import Radio from "../../components/radio/Radio";
-import { Table } from "../../components/table";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import PendingPaymentTable from "../../ui/customer_ui/PendingPaymentTable";
+import HistoryPaymentTable from "../../ui/customer_ui/HistoryPaymentTable";
+import RejectedPaymentTable from "../../ui/customer_ui/RejectedPaymentTable";
 
 export default function PaymentHistory() {
   const navigate = useNavigate();
   const [type, setType] = useState<RenderData>();
 
-  const columns = [
-    {
-      title: "Payment ID",
-      dataIndex: "name",
-      key: "name",
-    },
-    {
-      title: "Amount",
-      dataIndex: "amount",
-      key: "amount",
-    },
-    {
-      title: "Date",
-      dataIndex: "date",
-      key: "date",
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
-      key: "status",
-    },
-    {
-      title: "Action",
-      dataIndex: "action",
-      key: "action",
-    },
-  ];
-
   const onAddFund = () => {
     navigate("/customer/add-fund");
   };
 
+  const data = [
+    {
+      key: '1',
+      amount: '$100.00',
+      transactionalCode: 'TXN001',
+      requestDate: '2024-07-14',
+      status: 0
+    },
+    {
+      key: '2',
+      amount: '$200.00',
+      transactionalCode: 'TXN002',
+      requestDate: '2024-07-15',
+      status: 1
+    },
+    {
+      key: '3',
+      amount: '$300.00',
+      transactionalCode: 'TXN003',
+      requestDate: '2024-07-16',
+      status: 2
+    },
+  ];
+
   const renderTable = () => {
     switch (type?.type) {
       case "pending":
-        return <Table columns={columns} />;
+        return <PendingPaymentTable data={data}/>;
       case "history":
-        return <Table columns={columns} />;
+        return <HistoryPaymentTable data={data}/>;
+      case "rejected":
+          return <RejectedPaymentTable data={data}/>;
       default:
-        return <Table columns={columns} />;
+        return <HistoryPaymentTable data={data}/>;
     }
   };
 
@@ -71,6 +71,12 @@ export default function PaymentHistory() {
                 setType({ type: "history" });
               },
             },
+            {
+              text: "Rejected",
+              onClick: () => {
+                setType({ type: "rejected" });
+              },
+            },
           ]}
           activeRender={(item) => <OutlineButton text={item?.text} />}
           render={(item) => (
@@ -88,4 +94,4 @@ export default function PaymentHistory() {
   );
 }
 
-type RenderData = { type: "pending" | "history" };
+type RenderData = { type: "pending" | "history"  | "rejected"};
